7783e15febf7c9d12d70e2215855519495b069c5
יהלי כגן <yali@yhlys-MacBook-Pro.local>
2026-08-03 15:04:52 +0300

    Added remove spaces and tabs

--- src/helpers.c
+++ src/helpers.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "helpers.h"
+#include "error_handle.h"
 
-void f(void) {
-    printf("f");
+void remove_tabs(char *str)
+{
+    char *src = str;
+    char *dst = str;
+
+    while (*src) {
+        if (*src != '\t') {
+            *dst++ = *src;
+        }
+        src++;
+    }
+
+    *dst = '\0';
+}
+
+void remove_extra_whitespaces(char *str) {
+    int i, j = 0;
+
+    for (i = 0; str[i]; i++) {
+        if (str[i] != ' ' || (j > 0 && str[j - 1] != ' '))
+            str[j++] = str[i];
+    }
+
+    str[j] = '\0';
+}
+
+int check_line_length(char *line, char *filename, int line_counter) {
+    int ret_val = strchr(line, '\n') == NULL;
+    if(ret_val)
+        print_error(ERROR_CODE_1, filename, line_counter);
+    return ret_val;
 }
--- src/helpers.h
+++ src/helpers.h
@@ -1,1 +1,5 @@
-void f(void);
+void remove_tabs(char *str);
+
+void remove_extra_whitespaces(char *str);
+
+int check_line_length(char *line, char *filename, int line_counter);
