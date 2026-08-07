826089da0c016162e2e4a2af70a43eb1a30245ba
יהלי כגן <yali@yhlys-MacBook-Pro.local>
2026-08-07 15:17:49 +0300

    fix

--- src/grammar.c
+++ src/grammar.c
@@ -23,9 +23,12 @@ op_code OPCODES[] = {
     {"stop", 0}
 };
 
-char *REGS[] = {"@r0", "@r1", "@r2", "@r3", "@r4", "@r5", "@r6", "@r7"};
+char *REGS[] = {"$r0", "$r1", "$r2", "$r3", "$r4", "$r5", "$r6", "$r7",
+                "$r8", "$r9", "$r10", "$r11", "$r12", "$r13", "$r14", "$r15",
+                "$r16", "$r17", "$r18", "$r19", "$r20", "$r21", "$r22", "$r23",
+                "$r24", "$r25", "$r26", "$r27", "$r28", "$r29", "$r30", "$r31"};
 
-char *INSTUCTIONS[] = {".data", ".string", ".extern", ".entry"};
+char *INSTUCTIONS[] = {".dh", ".dw", ".db", ".asciz", ".extern", ".entry"};
 
 int is_instruction(char *str) {
     int i;
